37d3bfb3799eb958c74274f030d06cd57de82ea2
Oleksandr Shaposhnykov <94079420+Salmix1326@users.noreply.github.com>
2025-07-22 22:18:55 +0200

    Update apiRoutes.js

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,5 +1,5 @@
 // routes.js
-const API_BASE_URL = 'http://localhost:5000/api'
+const API_BASE_URL = 'https://admirable-quokka-7bcc82.netlify.app'
 
 export default {
   // GET: Отримати всіх вчителів
